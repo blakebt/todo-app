00aec4e4c480ea3de0e39f1955218872e9c28fce
blakebt <brenden.blake94@gmail.com>
2023-10-01 15:09:57 -0400

    added visual feedback for completed tasks

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -3,6 +3,7 @@ import { FaTrashCan, FaPencil } from 'react-icons/fa6'
 import useTasksContext from "../hooks/useTasksContext"
 import Button from "./Button"
 import EditForm from './EditForm'
+import Checkbox from './Checkbox'
 
 interface TaskItemProps {
   id: number,
@@ -11,6 +12,8 @@ interface TaskItemProps {
 
 const TaskItem: React.FC<TaskItemProps> = ({ id, text }) => {
   const [edit, setEdit] = useState<boolean>(false);
+  const [completed, setCompleted] = useState<boolean>(false)
+
   const context = useTasksContext();
 
   if(!context) return <div>No context</div>
@@ -21,17 +24,23 @@ const TaskItem: React.FC<TaskItemProps> = ({ id, text }) => {
     setEdit(true)
   }
 
-  const handleClick = () => {
+  const handleDeleteClick = () => {
     removeTask(id);
   }
 
+  /* If the edit button was clicked, show the edit form instead of the task item */
   let content = edit ? (
-    <EditForm id={id} text={text} setEdit={setEdit}/>
+    <EditForm id={id} text={text} setEdit={setEdit} />
   ) : (
     <div className="flex items-center justify-between px-3 py-2 bg-offwhite w-full rounded">
-      <FaPencil className='mx-2 cursor-pointer' onClick={handleEditClick}/>
-      {text}
-      <Button className='ml-4 rounded' onClick={handleClick} danger><FaTrashCan /></Button>
+      <Checkbox setCompleted={setCompleted}/>
+      <div className={`flex mx-4 ${completed ? 'line-through' : ''}`}>
+        {text}
+      </div>
+      <div className='flex items-center'>
+        <Button className='ml-5 rounded' onClick={handleDeleteClick} danger><FaTrashCan /></Button>
+        <Button className='mx-1 rounded' onClick={handleEditClick} danger><FaPencil /></Button>
+      </div>
     </div>
   )
 
